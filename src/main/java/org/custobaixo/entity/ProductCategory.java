d6a35c4a997c14f6ea249ccadd67dc3b20c67d9b
Felipe Oliveira <lippe637@gmail.com>
2025-09-24 20:34:28 -0300

    feat: implementar enum ProductCategory com categorias

---
+++ src/main/java/org/custobaixo/entity/ProductCategory.java
@@ -0,0 +1,24 @@
+package org.custobaixo.entity;
+
+public enum ProductCategory {
+    ELETRONICOS("Eletrônicos"),
+    PASSAGENS_AEREAS("Passagens Aéreas"),
+    HOTEIS("Hotéis"),
+    ROUPAS("Roupas"),
+    LIVROS("Livros"),
+    CASA_E_JARDIM("Casa e Jardim"),
+    ESPORTES("Esportes"),
+    SAUDE_E_BELEZA("Saúde e Beleza"),
+    AUTOMOTIVO("Automotivo"),
+    OUTROS("Outros");
+
+    private final String displayName;
+
+    ProductCategory(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
