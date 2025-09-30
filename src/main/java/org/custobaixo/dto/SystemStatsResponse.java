072ec27297fd631c680d5cf6f69465f79b7f6aae
Felipe Oliveira <lippe637@gmail.com>
2025-09-30 19:15:26 -0300

    feat: implementar DTO SystemStatsResponse

--- src/main/java/org/custobaixo/dto/SystemStatsResponse.java
+++ src/main/java/org/custobaixo/dto/SystemStatsResponse.java
@@ -1,4 +1,28 @@
 package org.custobaixo.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SystemStatsResponse {
+
+    private long totalActiveProducts;
+    private long electronicsCount;
+    private long flightsCount;
+    private long hotelsCount;
+    private long clothesCount;
+    private long booksCount;
+    private long homeGardenCount;
+    private long sportsCount;
+    private long healthBeautyCount;
+    private long automotiveCount;
+    private long othersCount;
+    private LocalDateTime lastUpdated;
 }
