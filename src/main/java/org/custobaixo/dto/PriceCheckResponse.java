39cd84a4bfb57565fe78412ab1d2f12d2d207591
Felipe Oliveira <lippe637@gmail.com>
2025-09-30 19:15:11 -0300

    feat: implementar DTO PriceCheckResponse

--- src/main/java/org/custobaixo/dto/PriceCheckResponse.java
+++ src/main/java/org/custobaixo/dto/PriceCheckResponse.java
@@ -1,4 +1,22 @@
 package org.custobaixo.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class PriceCheckResponse {
+    private Long productId;
+    private BigDecimal oldPrice;
+    private BigDecimal newPrice;
+    private BigDecimal targetPrice;
+    private Boolean priceChanged;
+    private Boolean targetReached;
+    private LocalDateTime lastChecked;
 }
