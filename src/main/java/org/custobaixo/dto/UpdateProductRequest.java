e7d69c16919ca2fcb3042a36a3adf22833a69ee9
Felipe Oliveira <lippe637@gmail.com>
2025-09-30 19:16:04 -0300

    feat: implementar DTO UpdateProductRequest

--- src/main/java/org/custobaixo/dto/UpdateProductRequest.java
+++ src/main/java/org/custobaixo/dto/UpdateProductRequest.java
@@ -1,4 +1,24 @@
 package org.custobaixo.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class UpdateProductRequest {
+
+    private String productName;
+
+    @DecimalMin(value = "0.01", message = "Preço alvo deve ser maior que zero")
+        private BigDecimal targetPrice;
+
+    private String productUrl;
 }
