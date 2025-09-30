3203f0731fe32b6672d935e4c9ffe58c4448c208
Felipe Oliveira <lippe637@gmail.com>
2025-09-30 19:14:57 -0300

    feat: implementar DTO HealthCheckResponse

--- src/main/java/org/custobaixo/dto/HealthCheckResponse.java
+++ src/main/java/org/custobaixo/dto/HealthCheckResponse.java
@@ -1,4 +1,20 @@
 package org.custobaixo.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class HealthCheckResponse {
+
+    private String status;
+    private LocalDateTime timestamp;
+    private String message;
 }
