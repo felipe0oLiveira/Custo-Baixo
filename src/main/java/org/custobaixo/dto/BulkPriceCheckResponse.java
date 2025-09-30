b04ed3d2b01ddfd7972a585e1672a75d900bcfd7
Felipe Oliveira <lippe637@gmail.com>
2025-09-30 19:14:49 -0300

    feat: implementar DTO BulkPriceCheckResponse

--- src/main/java/org/custobaixo/dto/BulkPriceCheckResponse.java
+++ src/main/java/org/custobaixo/dto/BulkPriceCheckResponse.java
@@ -1,4 +1,20 @@
 package org.custobaixo.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class BulkPriceCheckResponse {
+
+    private int totalProducts;
+    private LocalDateTime checkedAt;
+    private String message;
 }
